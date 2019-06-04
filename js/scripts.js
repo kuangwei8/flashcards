cd05081a3d9937dada81d37ff58b3cc75b86465e
Student <example@example.com>
2019-06-04 13:32:29 -0700

    commited file

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+
+  $(".clickable").click(function(){
+    $(".javas").toggle();
+  });
+
+
+  $(".clickable1").click(function(){
+    $(".operator").toggle();
+  });
+
+
+});
